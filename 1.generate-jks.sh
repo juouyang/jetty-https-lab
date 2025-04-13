33c808c789ed09101d59d575785788a3acfdd911
Ju Ouyang <juouyang@gmail.com>
2025-04-13 17:49:43 +0800

    ✨ feat(build): automate jks keystore generation and verification
    - enhance script to automatically generate and verify jks keystore
    - update p12 password
    - add commands to remove old keystore files
    - add command to list keystore content
    - add certificate to verify jetty https connection

--- 1.generate-jks.sh
+++ 1.generate-jks.sh
@@ -5,11 +5,12 @@ KEY=ssl/server.key
 P12=ssl/keystore.p12
 JKS=ssl/keystore.jks
 ALIAS=nexus
-P12PASS=Ci87654321
+P12PASS=53916262
 STOREPASS=password
 KEYPASS=password
 
 echo "[1/3] ➜ Exporting to PKCS#12 (.p12)"
+rm -f $P12
 openssl pkcs12 -export \
   -in $CERT \
   -inkey $KEY \
@@ -18,6 +19,7 @@ openssl pkcs12 -export \
   -passout pass:$P12PASS
 
 echo "[2/3] ➜ Converting .p12 to .jks"
+rm -f $JKS
 keytool -importkeystore \
   -srckeystore $P12 -srcstoretype PKCS12 -srcstorepass $P12PASS \
   -destkeystore $JKS -deststoretype JKS \
@@ -25,3 +27,4 @@ keytool -importkeystore \
   -destkeypass $KEYPASS
 
 echo "[3/3] ✔ Done. Keystore saved at $JKS"
+keytool -list -v -keystore $JKS -storepass $STOREPASS
--- 3.verify-jetty.sh
+++ 3.verify-jetty.sh
@@ -1,1 +1,4 @@
-curl -v --resolve jetty.dev.net:8443:127.0.0.1 https://jetty.dev.net:8443
+curl -v \
+    --resolve jetty.dev.net:8443:127.0.0.1 \
+    --cacert ssl/server.crt \
+    https://jetty.dev.net:8443
